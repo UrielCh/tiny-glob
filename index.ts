9d6e7923e42a5294dcf158d4844a9634cf009f51
UrielCh <uriel.chemouni@gmail.com>
2022-10-11 15:01:16 +0300

    publish  @u4/tiny-glob@0.2.9

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { BlobOptions } from './common';
+export { type BlobOptions } from './common';
 export { glob } from './async';
 export { glob as sync } from './sync';
 export { glob as async } from './async';
